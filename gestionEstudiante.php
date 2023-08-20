a2fcd0480d46490c45d2faccd3d709e6b9c7deab
Ignacio99P <panjose1104@gmail.com>
2023-08-20 13:16:59 -0300

    Arreglo de guardado y cargado

--- gestionEstudiante.php
+++ gestionEstudiante.php
@@ -1,9 +1,57 @@
 <?php
 class gestionEstudiante {
     private $estudiantes = [];
+
+    public function __construct() {
+        $this->cargarDesdeJSON();
+    }
+
     public function agregarEstudiante($estudiante) {
         $this->estudiantes[] = $estudiante;
+        $this->guardarEnJSON();
     } 
+
+    private function cargarDesdeJSON() {
+        if (file_exists('data.json')) {
+            $jsonData = file_get_contents('data.json');
+            $data = json_decode($jsonData, true);
+
+            if ($data && isset($data['estudiantes'])) {
+                foreach ($data['estudiantes'] as $estudianteData) {
+                    $this->estudiantes[] = $this->arrayToEstudiante($estudianteData);
+                }
+            }
+        }
+    }
+
+    private function arrayToEstudiante($estudianteData) {
+        return new Estudiante(
+            $estudianteData['nombre'],
+            $estudianteData['apellido'],
+            $estudianteData['dni'],
+            $estudianteData['email']
+        );
+    }
+
+    private function guardarEnJSON() {
+        $data = [
+            'estudiantes' => array_map([$this, 'estudianteToArray'], $this->estudiantes),
+        ];
+
+        $jsonData = json_encode($data, JSON_PRETTY_PRINT);
+        file_put_contents('data.json', $jsonData);
+    }
+
+    private function estudianteToArray($estudiante) {
+        return [
+            'nombre' => $estudiante->getNombre(),
+            'apellido' => $estudiante->getApellido(),
+            'dni' => $estudiante->getDNI(),
+            'email' => $estudiante->getEmail(),
+        ];
+    }
+
+
     public function eliminarEstudiantePorDNI($dni) {
         $indice = -1;
         foreach ($this->estudiantes as $key => $estudiante) {
@@ -15,23 +63,27 @@ class gestionEstudiante {
 
         if ($indice !== -1) {
             array_splice($this->estudiantes, $indice, 1);
+            $this->guardarEnJSON();
             return true;
         }
 
         return false;
     }
+    
     public function modificarEstudiantePorDNI($dni, $nuevoNombre, $nuevoApellido, $nuevoEmail) {
         foreach ($this->estudiantes as $estudiante) {
             if ($estudiante->getDNI() === $dni) {
                 $estudiante->setNombre($nuevoNombre);
                 $estudiante->setApellido($nuevoApellido);
                 $estudiante->setEmail($nuevoEmail);
+                $this->guardarEnJSON();
                 return true;
             }
         }
 
         return false;
     }
+
     public function buscarEstudiantePorDNI($dni) {
         foreach ($this->estudiantes as $estudiante) {
             if ($estudiante->getDNI() === $dni) {
